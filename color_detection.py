07044d6a16c7b9ff1cec1215f4fa0232eb596f13
galiyadaa <74764878+galiyadaa@users.noreply.github.com>
2023-12-13 17:26:09 +0600

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -21,3 +21,32 @@ red_value = green_value = blue_value = x_position = y_position = 0
 # The CSV file is assumed to have columns: "color", "color_name", "hex", "R", "G", "B"
 columns = ["color", "color_name", "hex", "R", "G", "B"]
 color_data = pd.read_csv('colors.csv', names=columns, header=None)
+
+def get_color_name(rgb, color_data):
+    """
+    Get the color name that is closest to the provided RGB values.
+
+    Parameters:
+    - rgb (tuple): The RGB values as a tuple (R, G, B).
+    - color_data (DataFrame): The DataFrame containing color information.
+
+    Returns:
+    - str: The name of the closest matching color.
+    """
+    min_distance = float('inf')  # Initialize with positive infinity
+    closest_color_name = "Unknown Color"
+
+    for i in range(len(color_data)):
+        color_rgb = color_data.loc[i, ["R", "G", "B"]].values
+        d = sum(abs(x - y) for x, y in zip(rgb, color_rgb))
+
+        if d < min_distance:
+            min_distance = d
+            closest_color_name = color_data.loc[i, "color_name"]
+
+    return closest_color_name
+
+# Example usage:
+input_color = (100, 150, 200)
+result_color_name = get_color_name(input_color, colors.csv)
+print(f"The closest matching color is: {result_color_name}")
